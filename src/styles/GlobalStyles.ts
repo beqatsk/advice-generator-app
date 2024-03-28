55bca382f0a52430112ccb93ba5dd892d58f52ed
beqa <beqa.tskhvediani@gmail.com>
2024-03-28 10:54:11 +0400

    add globalstyles

---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+ *{
+    margin: 0;
+    padding:  0;
+   box-sizing: border-box
+}
+body{
+    font-family: "Manrope", sans-serif;
+    min-height: 100vh;
+    background-color: darkcyan;
+}
+`;
+
+export default GlobalStyles;
